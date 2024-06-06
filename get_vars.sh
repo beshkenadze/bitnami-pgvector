c6910eb419f53becd21bcfd2e70828c68ad2ced6
hofq <jona.kieltsch@bat-bs.com>
2024-06-06 15:30:25 +0200

    Move envs to Action File

--- get_vars.sh
+++ get_vars.sh
@@ -5,8 +5,6 @@ wget https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64
 mv jq-linux-amd64 jq
 chmod +x jq
 
-PG_MAJOR_VERSION=16
-
 BITNAMI_NAME_REGEXP="^$PG_MAJOR_VERSION.*debian"
 echo $BITNAMI_NAME_REGEXP
 BITNAMI_POSTGRES_REG_CONTENT=$(wget -q -O - "https://hub.docker.com/v2/namespaces/bitnami/repositories/postgresql/tags?page_size=100")
@@ -19,7 +17,6 @@ PGVECTOR_NAME=$(echo -n $PGVECTOR_REG_CONTENT | jq -r '.results[] | select(.name
 PGVECTOR_DIGEST=$(echo -n $PGVECTOR_REG_CONTENT | jq -r '.results[] | select(.name | test(".*\\..*\\..*-pg16")) | .digest' | head -n 1)
 echo "PGVector - Name: $PGVECTOR_NAME, Digest: ${PGVECTOR_DIGEST:7:5}"
 
-IMAGE_NAME=bitnami-pgvector
 TAG_IDENTIFIER=$IMAGE_NAME:pg$PG_MAJOR_VERSION-${BITNAMI_DIGEST:7:5}-${PGVECTOR_DIGEST:7:5}
 echo "Identifier will be $TAG_IDENTIFIER"
 
@@ -30,9 +27,7 @@ fi
 
 # export vars for later jobs
 
-echo "IMAGE_NAME=$IMAGE_NAME" >> $GITHUB_ENV
 echo "TAG_IDENTIFIER=$TAG_IDENTIFIER" >> $GITHUB_ENV
-echo "PG_MAJOR_VERSION=$PG_MAJOR_VERSION" >> $GITHUB_ENV
 
 echo "PGVECTOR_NAME=$PGVECTOR_NAME" >> $GITHUB_ENV
 echo "BITNAMI_NAME=$BITNAMI_NAME" >> $GITHUB_ENV
