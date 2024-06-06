728c72824a2b0ec210327d823f19d3f40aeaf112
hofq <jona.kieltsch@bat-bs.com>
2024-06-06 14:15:23 +0200

    Testing5

--- get_vars.sh
+++ get_vars.sh
@@ -12,17 +12,10 @@ BITNAMI_NAME=$(echo -n $BITNAMI_POSTGRES_REG_CONTENT | jq -r --arg BITNAMI_NAME_
 BITNAMI_DIGEST=$(echo -n $BITNAMI_POSTGRES_REG_CONTENT | jq -r --arg BITNAMI_NAME_REGEXP $BITNAMI_NAME_REGEXP '.results[] | select(.name | test($BITNAMI_NAME_REGEXP)) | .digest' | head -n 1)
 echo "Bitnami - Name: $BITNAMI_NAME, Digest: ${BITNAMI_DIGEST:7:5}"
 
-echo "BITNAMI_NAME=$BITNAMI_NAME" >> $GITHUB_ENV
-echo "BITNAMI_DIGEST=${BITNAMI_DIGEST:7:5}"
-
-
 PGVECTOR_REG_CONTENT=$(wget -q -O - "https://hub.docker.com/v2/namespaces/pgvector/repositories/pgvector/tags?page_size=20")
 PGVECTOR_NAME=$(echo -n $PGVECTOR_REG_CONTENT | jq -r '.results[] | select(.name | test(".*\\..*\\..*-pg16")) | .name' | head -n 1)
 PGVECTOR_DIGEST=$(echo -n $PGVECTOR_REG_CONTENT | jq -r '.results[] | select(.name | test(".*\\..*\\..*-pg16")) | .digest' | head -n 1)
 echo "PGVector - Name: $PGVECTOR_NAME, Digest: ${PGVECTOR_DIGEST:7:5}"
-
-echo "PGVECTOR_NAME=$PGVECTOR_NAME" >> $GITHUB_ENV
-echo "PGVECTOR_DIGEST=${PGVECTOR_DIGEST:7:5}"
 
 IMAGE_NAME=bitnami-pgvector
 TAG_IDENTIFIER=$IMAGE_NAME:pg$PG_MAJOR_VERSION-{BITNAMI_DIGEST:7:5}-${PGVECTOR_DIGEST:7:5}
@@ -31,4 +24,12 @@ if curl --head --fail -H "Authorization: Bearer $GITHUB_TOKEN" https://ghcr.io/v
   echo "latest Tag found in Registry, no further build is required"
   exit 1
 fi
-# push with pg16 and pg16-digest-digest
+
+# export vars for later jobs
+
+echo "IMAGE_NAME=$IMAGE_NAME" >> $GITHUB_ENV
+echo "TAG_IDENTIFIER=$TAG_IDENTIFIER" >> $GITHUB_ENV
+echo "PG_MAJOR_VERSION=$PG_MAJOR_VERSION" >> $GITHUB_ENV
+
+echo "PGVECTOR_NAME=$PGVECTOR_NAME" >> $GITHUB_ENV
+echo "BITNAMI_NAME=$BITNAMI_NAME" >> $GITHUB_ENV
