4ddece565b288c1b3ccbf0c9f9848a5a01dab82e
hofq <jona.kieltsch@bat-bs.com>
2024-06-06 11:26:51 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM pgvector/pgvector:$PGVECTOR_NAME AS builder
+
+FROM bitnami/postgresql:$BITNAMI_NAME
+
+COPY --from=builder /usr/lib/postgresql/16/lib/vector.so /opt/bitnami/postgresql/lib/
+COPY --from=builder /usr/share/postgresql/16/extension/vector* /opt/bitnami/postgresql/share/extension/
---
+++ get_vars.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get Deps
+wget https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64
+mv jq-linux-amd64 jq
+chmod +x jq
+GITHUB_ENV=g
+PG_MAJOR_VERSION=16
+BITNAMI_NAME_REGEXP="^$PG_MAJOR_VERSION.*debian"
+BITNAMI_POSTGRES_REG_CONTENT=$(wget -q -O - "https://hub.docker.com/v2/namespaces/bitnami/repositories/postgresql/tags?page_size=20")
+BITNAMI_NAME=$(echo -n $BITNAMI_POSTGRES_REG_CONTENT | jq -r --arg BITNAMI_NAME_REGEXP $BITNAMI_NAME_REGEXP '.results[] | select(.name | test($BITNAMI_NAME_REGEXP)) | .name' | head -n 1)
+BITNAMI_DIGEST=$(echo -n $BITNAMI_POSTGRES_REG_CONTENT | jq -r --arg BITNAMI_NAME_REGEXP $BITNAMI_NAME_REGEXP '.results[] | select(.name | test($BITNAMI_NAME_REGEXP)) | .digest' | head -n 1)
+echo "Bitnami - Name: $BITNAMI_NAME, Digest: ${BITNAMI_DIGEST:7:5}"
+
+echo "BITNAMI_NAME=$BITNAMI_NAME" >> $GITHUB_ENV
+echo "BITNAMI_DIGEST=${BITNAMI_DIGEST:7:5}"
+
+
+PGVECTOR_REG_CONTENT=$(wget -q -O - "https://hub.docker.com/v2/namespaces/pgvector/repositories/pgvector/tags?page_size=20")
+PGVECTOR_NAME=$(echo -n $PGVECTOR_REG_CONTENT | jq -r '.results[] | select(.name | test(".*\\..*\\..*-pg16")) | .name' | head -n 1)
+PGVECTOR_DIGEST=$(echo -n $PGVECTOR_REG_CONTENT | jq -r '.results[] | select(.name | test(".*\\..*\\..*-pg16")) | .digest' | head -n 1)
+echo "PGVector - Name: $PGVECTOR_NAME, Digest: ${PGVECTOR_DIGEST:7:5}"
+
+echo "PGVECTOR_NAME=$PGVECTOR_NAME" >> $GITHUB_ENV
+echo "PGVECTOR_DIGEST=${PGVECTOR_DIGEST:7:5}"
+
+LATEST_TAG=$bitnami-pgvector:pg$PG_MAJOR_VERSION-{BITNAMI_DIGEST:7:5}-${PGVECTOR_DIGEST:7:5}"
+
+curl --head --fail -H "Authorization: Bearer $TOKEN" https://ghcr.io/v2/bat-bs/bitnami-pgvector/manifests/$LATEST_TAG
+# push with pg16 and pg16-digest-digest
