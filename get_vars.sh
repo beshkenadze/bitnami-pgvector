78018782a479b0f99faa7070017b7cad02d80803
hofq <jona.kieltsch@bat-bs.com>
2024-06-06 11:37:20 +0200

    Testing

--- get_vars.sh
+++ get_vars.sh
@@ -26,5 +26,5 @@ echo "PGVECTOR_DIGEST=${PGVECTOR_DIGEST:7:5}"
 
 LATEST_TAG=$bitnami-pgvector:pg$PG_MAJOR_VERSION-{BITNAMI_DIGEST:7:5}-${PGVECTOR_DIGEST:7:5}"
 
-curl --head --fail -H "Authorization: Bearer $TOKEN" https://ghcr.io/v2/bat-bs/bitnami-pgvector/manifests/$LATEST_TAG
+curl --head --fail -H "Authorization: Bearer $GITHUB_TOKEN" https://ghcr.io/v2/bat-bs/bitnami-pgvector/manifests/$LATEST_TAG
 # push with pg16 and pg16-digest-digest
